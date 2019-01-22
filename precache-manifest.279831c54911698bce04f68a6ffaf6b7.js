ef287af1614233fa7adabd337caf99da1a7ee05f
saniok017 <saniok017@gmail.com>
2019-01-22 19:53:10 +0300

    Updates

---
+++ precache-manifest.279831c54911698bce04f68a6ffaf6b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "116d6ea4f0e4f505c82f",
+    "url": "/ReactTest/static/js/runtime~main.116d6ea4.js"
+  },
+  {
+    "revision": "14039d91eb144823dc81",
+    "url": "/ReactTest/static/js/main.14039d91.chunk.js"
+  },
+  {
+    "revision": "ec8eef1e198ebb0cbe4b",
+    "url": "/ReactTest/static/js/1.ec8eef1e.chunk.js"
+  },
+  {
+    "revision": "14039d91eb144823dc81",
+    "url": "/ReactTest/static/css/main.b89561de.chunk.css"
+  },
+  {
+    "revision": "e835f829036b411487e9d654acdf67a3",
+    "url": "/ReactTest/index.html"
+  }
+];
